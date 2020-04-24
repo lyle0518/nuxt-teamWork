cf4c8c2837b800bd8444f00e286fa5b65f04d6bf
Lyle <494205736@qq.com>
2020-04-24 19:33:56 +0800

    '新增订单页面,完成新增乘机人和移除乘机人功能'

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -2,29 +2,43 @@
   <div class="flight-item">
     <div>
       <!-- 显示的机票信息 -->
-      <el-row type="flex" align="middle" class="flight-info" @click.native="isShow=!isShow">
+      <el-row
+        type="flex"
+        align="middle"
+        class="flight-info"
+        @click.native="isShow = !isShow"
+      >
         <el-col :span="6">
-          <span>{{data.airline_name}}</span>
-          {{data.flight_no}}
+          <span>{{ data.airline_name }}</span>
+          {{ data.flight_no }}
         </el-col>
         <el-col :span="12">
-          <el-row type="flex" justify="space-between" class="flight-info-center">
+          <el-row
+            type="flex"
+            justify="space-between"
+            class="flight-info-center"
+          >
             <el-col :span="8" class="flight-airport">
-              <strong>{{data.dep_time}}</strong>
-              <span>{{data.org_airport_name}}{{data.org_airport_quay}}</span>
+              <strong>{{ data.dep_time }}</strong>
+              <span
+                >{{ data.org_airport_name }}{{ data.org_airport_quay }}</span
+              >
             </el-col>
             <el-col :span="8" class="flight-time">
-              <span>{{rankTime}}</span>
+              <span>{{ rankTime }}</span>
             </el-col>
             <el-col :span="8" class="flight-airport">
-              <strong>{{data.arr_time}}</strong>
-              <span>{{data.dst_airport_name}}{{data.dst_airport_quay}}</span>
+              <strong>{{ data.arr_time }}</strong>
+              <span
+                >{{ data.dst_airport_name }}{{ data.dst_airport_quay }}</span
+              >
             </el-col>
           </el-row>
         </el-col>
         <el-col :span="6" class="flight-info-right">
           ￥
-          <span class="sell-price">{{data.base_price / 2}}</span>起
+          <span class="sell-price">{{ data.base_price / 2 }}</span
+          >起
         </el-col>
       </el-row>
     </div>
@@ -38,16 +52,26 @@
             justify="space-between"
             align="middle"
             class="flight-sell"
-            v-for="(item,index) in data.seat_infos"
+            v-for="(item, index) in data.seat_infos"
             :key="index"
           >
             <el-col :span="16" class="flight-sell-left">
-              <span>{{item.name}}</span>
-              | {{item.supplierName}}
+              <span>{{ item.name }}</span>
+              | {{ item.supplierName }}
             </el-col>
-            <el-col :span="5" class="price">￥{{item.settle_price}}</el-col>
+            <el-col :span="5" class="price">￥{{ item.settle_price }}</el-col>
             <el-col :span="3" class="choose-button">
-              <el-button type="warning" size="mini">选定</el-button>
+              <nuxt-link
+                :to="{
+                  path: '/air/order',
+                  query: {
+                    air: data.id,
+                    seat_xid: item.seat_xid
+                  }
+                }"
+              >
+                <el-button type="warning" size="mini">选定</el-button>
+              </nuxt-link>
               <p>剩余：83</p>
             </el-col>
           </el-row>
