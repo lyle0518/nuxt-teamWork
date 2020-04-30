b14ab122936a36c1f21a7dc0e3f94fd692016cf9
Lyle <494205736@qq.com>
2020-05-01 01:11:07 +0800

    '实现表单的筛选功能,实现首次进入定位'

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -6,8 +6,85 @@
       <span>></span>
       <nuxt-link to="#">酒店预订</nuxt-link>
     </div>
-    <!-- 封装单独的组件  第一个筛选表单 -->
-    <HotelSeachForm />
+    <!--   第一个筛选表单 -->
+    <div class="filter">
+      <el-form ref="form" :inline="true" :model="form" label-width="80px" v-model="form">
+        <!-- 城市 -->
+        <el-form-item>
+          <el-autocomplete
+            v-model="form.cityName"
+            :fetch-suggestions="querySearch"
+            placeholder="切换城市"
+            @select="handleSelect"
+            style="margin-right:10px"
+          ></el-autocomplete>
+        </el-form-item>
+        <!-- 时间 -->
+        <el-form-item>
+          <el-date-picker
+            v-model="time"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            value-format="yyyy-MM-dd"
+            :picker-options="pickerOptions"
+          ></el-date-picker>
+        </el-form-item>
+        <!-- 乘坐人 -->
+        <el-form-item>
+          <el-popover placement="bottom" width="326" v-model="visible">
+            <div class="people" style=" border-bottom: 1px solid #ddd;padding-bottom:20px">
+              <!-- 多选 -->
+              <span style="margin-right:80px">每间</span>
+              <span class="select1">
+                <el-select v-model="value1" size="mini" style="width:93px">
+                  <el-option
+                    v-for="(item,index) in options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                  <!-- 定位--大人字样 -->
+                </el-select>
+              </span>
+              <span>
+                <el-select
+                  v-model="value2"
+                  size="mini"
+                  style="margin-left: 10px;
+            width:93px"
+                  class="select2"
+                >
+                  <el-option
+                    v-for="(item,index) in options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                  ></el-option>
+                </el-select>
+              </span>
+            </div>
+            <div style="text-align: right; margin-top: 20px">
+              <el-button type="primary" size="mini" @click="setPeople">确定</el-button>
+            </div>
+            <el-input
+              style="width:200px"
+              placeholder="未定人数"
+              slot="reference"
+              suffix-icon="el-icon-user"
+              v-model="peopleNum"
+              readonly="readonly"
+            ></el-input>
+          </el-popover>
+        </el-form-item>
+        <!-- 提交 -->
+        <el-form-item>
+          <el-button type="primary" @click="submit">查找酒店</el-button>
+        </el-form-item>
+        <!-- 确认按钮 -->
+      </el-form>
+    </div>
     <!-- 地图部分 -->
     <el-row style="height:260px">
       <el-col :span="14">
@@ -158,21 +235,92 @@
 
 <script>
 import HotelItem from "@/components/hotel/hotelItem";
-import HotelSeachForm from "@/components/hotel/hotelSeachForm";
 
 export default {
   beforeRouteUpdate(to, from, next) {
     console.log("触发");
     next();
     //  重新请求酒店数据
+    this.form.cityName = this.$route.query.cityName;
+    // this.$store.commit("hotel/setHotelForm", );
     this.getList();
   },
   components: {
-    HotelItem,
-    HotelSeachForm
+    HotelItem
   },
   data() {
     return {
+      //第一个删选框的参数
+      form: {
+        //切换城市value值
+        cityName: "",
+        //入店时间
+        enterTime: "",
+        //;离店
+        leftTime: ""
+      },
+      //条数
+      _limit: 1,
+      //开始页数
+      _start: 0,
+      //入住人数
+      person: 1,
+      // 城市的id-请求参数
+      city: "",
+
+      //时间
+      time: "",
+      //时期禁用
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() < Date.now() - 3600 * 1000 * 24;
+        }
+      },
+      //乘坐人框显示/隐藏
+      visible: false,
+      // 乘坐人成人数据
+      select1: "",
+      // 乘坐人小孩数据
+      select2: "",
+      //乘车人框的下拉选项
+      options: [
+        {
+          value: "1",
+          label: "1"
+        },
+        {
+          value: "2",
+          label: "2"
+        },
+        {
+          value: "3",
+          label: "3"
+        },
+        {
+          value: "4",
+          label: "4"
+        },
+        {
+          value: "5",
+          label: "5"
+        },
+        {
+          value: "6",
+          label: "6"
+        },
+        {
+          value: "7",
+          label: "7"
+        }
+      ],
+      //乘车人成人下拉框绑定值
+      value1: "",
+      //乘车人小孩下拉框绑定值
+      value2: "",
+      // 乘车框value值
+      peopleNum: "",
+
+      //分割线---
       value1: "",
       value2: "",
       time: "",
@@ -234,6 +382,70 @@ export default {
     this.getList();
   },
   methods: {
+    //筛选框的方法
+    querySearch(value, cb) {
+      if (!value) {
+        cb([]);
+        return;
+      }
+      this.$axios({
+        url: "/cities",
+        params: {
+          name: value
+        }
+      }).then(res => {
+        const { data } = res.data;
+        const list = data.map(v => {
+          v.value = v.name;
+          return v;
+        });
+
+        cb(list);
+      });
+    },
+    //城市下拉触发
+    handleSelect(item) {
+      this.form.cityName = item.name;
+      this.city = item.id;
+      // 传递当前form给仓库
+      // 修改this.form里面的内容
+      const { cityName, ...other } = this.form;
+      other.city = this.city;
+      this.$store.commit("hotel/setHotelForm", other);
+      this.$router.push({
+        path: "/hotel",
+        query: {
+          cityName: this.form.cityName
+        }
+      });
+    },
+    //乘车人框确定事件
+    setPeople() {
+      this.visible = false;
+      this.peopleNum = `${this.value1}成人`;
+    },
+    //提交请求
+    submit() {
+      console.log(111);
+      // 处理入店时间和离开事件
+      if (!this.time) return;
+      this.form.enterTime = this.time[0];
+      this.form.leftTime = this.time[1];
+      // 跳转页面
+      console.log(this.form);
+      //
+      this.$router.push({
+        path: "/hotel",
+        query: this.form
+      });
+
+      //将表单存到仓库 此时form城市id值不对
+      const { cityName, ...other } = this.form;
+      other.city = this.city;
+      this.$store.commit("hotel/setHotelForm", other);
+    },
+
+    //分割线
     //子组件传递form参数给父组件请求酒店数据
     //data为form表单数据
     getList() {
@@ -241,7 +453,6 @@ export default {
         url: "/hotels",
         params: this.$store.state.hotel.hotelForm
       }).then(res => {
-        console.log(res);
         const { data } = res.data;
         // 存进公共仓库
         this.$store.commit("hotel/setHotelList", data);
@@ -250,12 +461,10 @@ export default {
     //获取下拉框的index
     getIndex(index) {
       //外层框的index
-      console.log("index:" + index);
       this.levIndex = index;
     },
     //选择下拉列表
     handleSelectLev(item1, index) {
-      console.log(item1, index);
       // 查找是否已有值
       const res = this.hoteltype[index].selectLev.indexOf(item1.label);
       if (res > -1) {
@@ -265,7 +474,6 @@ export default {
         // 没有值-
         this.hoteltype[index].selectLev.push(item1.label);
       }
-      console.log(this.hoteltype[index].selectLev);
 
       //修改不限的值
       if (this.hoteltype[index].selectLev.length > 1) {
@@ -294,11 +502,11 @@ export default {
               name: this.mapCity
             }
           }).then(res => {
-            console.log(res);
             const { data } = res.data;
             data.forEach(v => {
               if (v.name === this.mapCity) {
-                console.log(v.id);
+                // 备份一份当前的id用作使用
+                this.cityId = v.id;
                 this.$store.commit("hotel/setHotelFormCity", v.id);
                 this.getList();
               }
@@ -350,6 +558,9 @@ export default {
   mounted() {
     // this.open();
     // 判断cityname是否有值
+    if (this.$route.query.cityName && this.form.cityName === "") {
+      this.form.cityName = this.$route.query.cityName;
+    }
     this.getMap();
     setTimeout(() => {
       // 请求酒店的数据
